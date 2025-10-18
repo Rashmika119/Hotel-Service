667293e691cb71ce9735c0de42373e8aceb35c06
Rashmika119 <rashmikanethsarni119@gmail.com>
2025-10-18 11:59:51 +0530

    completed the functionalities in hotel service

---
+++ src/hotel.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { HotelController } from './hotel.controller';
+import { HotelService } from './hotel.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hotel } from './hotel.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'hotel.sqlite',
+      entities: [Hotel],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Hotel])
+  ],
+  controllers: [HotelController],
+  providers: [HotelService],
+})
+export class AppModule { }
